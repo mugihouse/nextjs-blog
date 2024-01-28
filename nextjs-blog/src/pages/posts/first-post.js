f2f0f284371b50c2feacf8897c30236d53f7b63f
mugihouse <mugi.stnk100122@gmail.com>
2024-01-28 12:16:00 +0900

    Add: リンク作成

--- nextjs-blog/src/pages/posts/first-post.js
+++ nextjs-blog/src/pages/posts/first-post.js
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Create Post</title>
+      </Head>
       <h1>First Post</h1>
       <Link href="/">Back to home</Link>
     </>
